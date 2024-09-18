bee8b5336e20fbb6b2114820c4d0fb475c2be15a
Luca <ngtruongthanh98@gmail.com>
2024-09-18 19:02:54 +0700

    feat: add initial project files and configuration

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+const msg = ref('Hello world');
+</script>
+
+<template>
+  <div>{{ msg }}</div>
+</template>
+
+<style scoped lang="scss"></style>
