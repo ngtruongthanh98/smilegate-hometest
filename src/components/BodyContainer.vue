06d3515b053476b1c8002abda9f408499e6bfddb
ThanhNT26 <thanhnt26@vng.com.vn>
2024-09-20 09:34:15 +0700

    refactor: Update BodyContainer to use MOCK_DATA_PHOTO_LIST for image data

--- src/components/BodyContainer.vue
+++ src/components/BodyContainer.vue
@@ -1,6 +1,7 @@
 <script>
 import HeaderTitle from './HeaderTitle.vue';
 import ImageCardList from './ImageCardList.vue';
+import { MOCK_DATA_PHOTO_LIST } from '../constants'
 
 export default {
   name: 'BodyContainer',
@@ -10,28 +11,7 @@ export default {
   },
   data() {
     return {
-      imageData: [
-        { src: 'https://i.imgur.com/unmma82.jpeg', alt: 'Placeholder Image 1', title: 'Product name is quite long in some cases.' },
-        { src: 'https://i.imgur.com/2MM9Jqo.png', alt: 'Placeholder Image 2', title: 'Product name is quite long in some cases.' },
-        { src: 'https://i.imgur.com/unmma82.jpeg', alt: 'Placeholder Image 3', title: 'Product name is quite long in some cases.' },
-        { src: 'https://i.imgur.com/2MM9Jqo.png', alt: 'Placeholder Image 4', title: 'Product name is quite long in some cases.' },
-        { src: 'https://i.imgur.com/unmma82.jpeg', alt: 'Placeholder Image 5', title: 'Product name is quite long in some cases.' },
-        { src: 'https://i.imgur.com/2MM9Jqo.png', alt: 'Placeholder Image 6', title: 'Product name is quite long in some cases.' },
-        { src: 'https://i.imgur.com/unmma82.jpeg', alt: 'Placeholder Image 7', title: 'Product name is quite long in some cases.' },
-        { src: 'https://i.imgur.com/2MM9Jqo.png', alt: 'Placeholder Image 8', title: 'Product name is quite long in some cases.' },
-        { src: 'https://i.imgur.com/unmma82.jpeg', alt: 'Placeholder Image 9', title: 'Product name is quite long in some cases.' },
-        { src: 'https://i.imgur.com/2MM9Jqo.png', alt: 'Placeholder Image 10', title: 'Product name is quite long in some cases.' },
-        { src: 'https://i.imgur.com/unmma82.jpeg', alt: 'Placeholder Image 11', title: 'Product name is quite long in some cases.' },
-        { src: 'https://i.imgur.com/2MM9Jqo.png', alt: 'Placeholder Image 12', title: 'Product name is quite long in some cases.' },
-        { src: 'https://i.imgur.com/unmma82.jpeg', alt: 'Placeholder Image 13', title: 'Product name is quite long in some cases.' },
-        { src: 'https://i.imgur.com/2MM9Jqo.png', alt: 'Placeholder Image 14', title: 'Product name is quite long in some cases.' },
-        { src: 'https://i.imgur.com/unmma82.jpeg', alt: 'Placeholder Image 15', title: 'Product name is quite long in some cases.' },
-        { src: 'https://i.imgur.com/2MM9Jqo.png', alt: 'Placeholder Image 16', title: 'Product name is quite long in some cases.' },
-        { src: 'https://i.imgur.com/unmma82.jpeg', alt: 'Placeholder Image 17', title: 'Product name is quite long in some cases.' },
-        { src: 'https://i.imgur.com/2MM9Jqo.png', alt: 'Placeholder Image 18', title: 'Product name is quite long in some cases.' },
-        { src: 'https://i.imgur.com/unmma82.jpeg', alt: 'Placeholder Image 19', title: 'Product name is quite long in some cases.' },
-        { src: 'https://i.imgur.com/2MM9Jqo.png', alt: 'Placeholder Image 20', title: 'Product name is quite long in some cases.' }
-      ]
+      imageData: MOCK_DATA_PHOTO_LIST
     }
   }
 };
---
+++ src/constants/index.ts
@@ -0,0 +1,22 @@
+export const MOCK_DATA_PHOTO_LIST = [
+  { src: 'https://i.imgur.com/unmma82.jpeg', alt: 'Placeholder Image 1', title: 'Product name is quite long in some cases.' },
+  { src: 'https://i.imgur.com/2MM9Jqo.png', alt: 'Placeholder Image 2', title: 'Product name is quite long in some cases.' },
+  { src: 'https://i.imgur.com/unmma82.jpeg', alt: 'Placeholder Image 3', title: 'Product name is quite long in some cases.' },
+  { src: 'https://i.imgur.com/2MM9Jqo.png', alt: 'Placeholder Image 4', title: 'Product name is quite long in some cases.' },
+  { src: 'https://i.imgur.com/unmma82.jpeg', alt: 'Placeholder Image 5', title: 'Product name is quite long in some cases.' },
+  { src: 'https://i.imgur.com/2MM9Jqo.png', alt: 'Placeholder Image 6', title: 'Product name is quite long in some cases.' },
+  { src: 'https://i.imgur.com/unmma82.jpeg', alt: 'Placeholder Image 7', title: 'Product name is quite long in some cases.' },
+  { src: 'https://i.imgur.com/2MM9Jqo.png', alt: 'Placeholder Image 8', title: 'Product name is quite long in some cases.' },
+  { src: 'https://i.imgur.com/unmma82.jpeg', alt: 'Placeholder Image 9', title: 'Product name is quite long in some cases.' },
+  { src: 'https://i.imgur.com/2MM9Jqo.png', alt: 'Placeholder Image 10', title: 'Product name is quite long in some cases.' },
+  { src: 'https://i.imgur.com/unmma82.jpeg', alt: 'Placeholder Image 11', title: 'Product name is quite long in some cases.' },
+  { src: 'https://i.imgur.com/2MM9Jqo.png', alt: 'Placeholder Image 12', title: 'Product name is quite long in some cases.' },
+  { src: 'https://i.imgur.com/unmma82.jpeg', alt: 'Placeholder Image 13', title: 'Product name is quite long in some cases.' },
+  { src: 'https://i.imgur.com/2MM9Jqo.png', alt: 'Placeholder Image 14', title: 'Product name is quite long in some cases.' },
+  { src: 'https://i.imgur.com/unmma82.jpeg', alt: 'Placeholder Image 15', title: 'Product name is quite long in some cases.' },
+  { src: 'https://i.imgur.com/2MM9Jqo.png', alt: 'Placeholder Image 16', title: 'Product name is quite long in some cases.' },
+  { src: 'https://i.imgur.com/unmma82.jpeg', alt: 'Placeholder Image 17', title: 'Product name is quite long in some cases.' },
+  { src: 'https://i.imgur.com/2MM9Jqo.png', alt: 'Placeholder Image 18', title: 'Product name is quite long in some cases.' },
+  { src: 'https://i.imgur.com/unmma82.jpeg', alt: 'Placeholder Image 19', title: 'Product name is quite long in some cases.' },
+  { src: 'https://i.imgur.com/2MM9Jqo.png', alt: 'Placeholder Image 20', title: 'Product name is quite long in some cases.' }
+]
